12b93e3ff717ded869fa6d31ea465f0c14b27af4
Olga Stepanova <lechateliermail@gmail.com>
2023-03-16 00:35:17 +0400

    feat: highlight the current page in the router

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import LogoSvg from '../../assets/svg/logo.svg';
 import './header.css';
 
+enum RouterPathes {
+  HOME = '/',
+  ABOUT = '/about',
+}
+
 export function Header() {
+  const [active, setActiveLink] = useState('/');
+
   return (
     <header className="header">
       <div className="header-content content-wrapper">
@@ -12,10 +19,18 @@ export function Header() {
           <span>Plants</span>
         </div>
         <div className="links">
-          <Link to="/" className="links-item">
+          <Link
+            to={RouterPathes.HOME}
+            className={`links-item ${active === RouterPathes.HOME ? 'active-path' : ''}`}
+            onClick={() => setActiveLink(RouterPathes.HOME)}
+          >
             Home
           </Link>
-          <Link to="/about" className="links-item">
+          <Link
+            to={RouterPathes.ABOUT}
+            className={`links-item ${active === RouterPathes.ABOUT ? 'active-path' : ''}`}
+            onClick={() => setActiveLink(RouterPathes.ABOUT)}
+          >
             About us
           </Link>
         </div>
