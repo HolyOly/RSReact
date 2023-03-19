4dabf4734f75b69ff9565ffc8aa7fe7163869873
Olga Stepanova <lechateliermail@gmail.com>
2023-03-19 06:34:50 +0300

    feat: add about content and refactor

--- src/components/sectionWelcome/welcome.tsx
+++ src/components/sectionWelcome/welcome.tsx
@@ -12,7 +12,7 @@ export function Welcome() {
         <h1 className="welcome-title">
           We grow <span className="colored">plants</span> and give you oxygen
         </h1>
-        <p className="welcome-subtitle">
+        <p className="welcome-subtitle ordinary-text">
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
           been the industry&apos;s standard dummy text ever since the 1500s.
         </p>
