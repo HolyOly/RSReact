8aa580b89231309192bf772d8d8790287a1a0f95
Olga Stepanova <lechateliermail@gmail.com>
2023-04-01 11:51:21 +0300

    fix: fix tests

--- src/components/contacts/contact.test.tsx
+++ src/components/contacts/contact.test.tsx
@@ -8,33 +8,26 @@ import { formStateInitial, warningsInitial } from '../../data/initial_data';
 import assert from 'assert';
 
 describe('Form', () => {
+  beforeEach(() => render(<Contacts {...testFormState} />));
   test('the existence of input text element', () => {
-    render(<Contacts {...testFormState} />);
-
     const inputEl = screen.getByTestId('name-input');
     expect(inputEl).toBeInTheDocument();
     expect(inputEl).toHaveAttribute('type', 'text');
   });
 
   test('the existence of input date element', () => {
-    render(<Contacts {...testFormState} />);
-
     const inputEl = screen.getByTestId('date-input');
     expect(inputEl).toBeInTheDocument();
     expect(inputEl).toHaveAttribute('type', 'date');
   });
 
   test('the existence of select element', () => {
-    render(<Contacts {...testFormState} />);
-
     const inputEl = screen.getByTestId('select-element');
     expect(inputEl).toBeInTheDocument();
     expect(inputEl).toHaveAttribute('name', 'country');
   });
 
   test('the existence of input checkox and radio input elements', () => {
-    render(<Contacts {...testFormState} />);
-
     const inputNotification = screen.getByTestId('checkbox-input');
     const inputMale = screen.getByTestId('male-input');
     const inputfemale = screen.getByTestId('female-input');
@@ -49,16 +42,12 @@ describe('Form', () => {
   });
 
   test('the existence of input file element', () => {
-    render(<Contacts {...testFormState} />);
-
     const inputfile = screen.getByLabelText('Choose profile picture');
     expect(inputfile).toBeInTheDocument();
     expect(inputfile).toHaveAttribute('type', 'file');
   });
 
   test('pass valid value to test input name field', async () => {
-    render(<Contacts {...testFormState} />);
-
     const inputEl = screen.getByTestId('name-input');
     await userEvent.type(inputEl, 'John Doe');
     const myTextAreaVal = (inputEl as HTMLInputElement).value;
@@ -66,8 +55,6 @@ describe('Form', () => {
   });
 
   test('pass valid value to test input date field', async () => {
-    render(<Contacts {...testFormState} />);
-
     const inputEl = screen.getByTestId('date-input');
     await userEvent.type(inputEl, '1996-12-04');
     const myTextAreaVal = (inputEl as HTMLInputElement).value;
@@ -75,7 +62,6 @@ describe('Form', () => {
   });
 
   test('pass valid value to test select field', async () => {
-    render(<Contacts {...testFormState} />);
     const selectElem = screen.queryByTestId('select-element') as HTMLSelectElement;
     await userEvent.selectOptions(selectElem, 'Turkey');
     expect((screen.getByText('Turkey') as HTMLOptionElement).selected).toBeTruthy();
@@ -83,20 +69,17 @@ describe('Form', () => {
   });
 
   test('test submit click', async () => {
-    render(<Contacts {...testFormState} />);
     const submitBtn = screen.getByText('Submit') as HTMLButtonElement;
     fireEvent.click(submitBtn);
   });
 
   test('test select file click', async () => {
-    render(<Contacts {...testFormState} />);
     const submitBtn = screen.getByText('Select file') as HTMLButtonElement;
     fireEvent.click(submitBtn);
   });
 
   test('test photo upload', async () => {
     const file = new File(['hellomyfriend'], 'myimg.png', { type: 'image/png' });
-    render(<Contacts {...testFormState} />);
 
     const fileInput = screen.getByLabelText('Choose profile picture');
     await userEvent.upload(fileInput, file).then(() => {
@@ -105,7 +88,6 @@ describe('Form', () => {
   });
 
   test('test', async () => {
-    render(<Contacts {...testFormState} />);
     if (testFormState.submitStatus === 'success') {
       const { container } = render(<Modal mode={'success'} text={''} />);
       expect(container.firstChild).toHaveClass('overlay');
@@ -113,7 +95,7 @@ describe('Form', () => {
   });
 
   it('test formStateInitial', () => {
-    assert.equal(formStateInitial.warnings.inputName, '');
+    assert.equal(formStateInitial.isValid, false);
     assert.equal(formStateInitial.submitStatus, 'pending');
   });
 
