7c1f7659885db4fdf6c78818920cbf26ff5315ca
Olga Stepanova <lechateliermail@gmail.com>
2023-04-23 19:45:20 +0300

    fix: fix cypress types

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,11 +1,8 @@
+/// <reference types="cypress" />
+
 describe('example to-do app', () => {
   it('can filter for uncompleted tasks', () => {
     cy.visit('/');
     cy.get('input').should('have.text', '');
   });
-
-  // it('can filter for uncompleted tasks', () => {
-  //   cy.visit('/');
-  //   cy.get('select').should('have.value', 'Choose a location');
-  // });
 });
